a1c0ba16dcf9a08d255de8da3d44c2bfc327d2b9
Shivam Bharti <shivam.bharti99@gmail.com>
2022-03-19 13:28:33 +0530

    optimized code

--- index.js
+++ index.js
@@ -10,16 +10,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors({ methods: ["GET", "POST"] }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  next();
-});
-
 app.use("/api/customers", customerRoutes);
 app.use("/api/history", historyRoutes);
 
